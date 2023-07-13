f0354d930c8cabd3715a9b72ee30648dfaeb087d
Akshaypmna18 <akshaypmna18@gmail.com>
2023-07-13 21:01:09 +0530

    updated files

---
+++ small/to-do/script.js
@@ -0,0 +1,35 @@
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+var input = document.getElementById("input-box");
+
+btn.addEventListener("click", function () {
+  var newListItem = document.createElement("li");
+  if (input.value != "") {
+    var textNode = document.createTextNode(input.value);
+  } else {
+    alert("Enter a task");
+  }
+  newListItem.appendChild(textNode);
+  list.appendChild(newListItem);
+});
+document.addEventListener("keydown", function (e) {
+  var newListItem = document.createElement("li");
+  var keyCode = e.keyCode;
+  if (keyCode == 13) {
+    if (input.value != "") {
+      var textNode = document.createTextNode(input.value);
+    } else {
+      alert("Enter a task");
+    }
+  }
+  newListItem.appendChild(textNode);
+  list.appendChild(newListItem);
+});
+
+list.addEventListener("click", function (e) {
+  var element = e.target;
+  element.style.backgroundColor = "#ee2e31";
+  setTimeout(function () {
+    element.remove();
+  }, 1000);
+});
