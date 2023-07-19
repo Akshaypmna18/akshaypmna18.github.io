914119b5476b3d98e6abf10cd9bce245a0ebe03b
Akshaypmna18 <akshaypmna18@gmail.com>
2023-07-19 08:32:48 +0530

    updated

--- small/music-player/script.js
+++ small/music-player/script.js
@@ -1,15 +1,18 @@
 var playBtn = document.getElementById("play-btn");
 var pauseBtn = document.getElementById("pause-btn");
 var katy = document.getElementById("katy-music");
+var songName = document.getElementById("song-name");
 katy.volume = 0.5;
 
 playBtn.onclick = function () {
   playBtn.classList.toggle("display");
   pauseBtn.classList.toggle("display");
+  songName.classList.toggle("anim");
   katy.play();
 };
 pauseBtn.onclick = function () {
   pauseBtn.classList.toggle("display");
   playBtn.classList.toggle("display");
+  songName.classList.toggle("anim");
   katy.pause();
 };
