eb772a17a68feed142297afc821c94286ab2b80a
Akshaypmna18 <akshaypmna18@gmail.com>
2023-07-21 12:41:27 +0530

    updates

--- small/watch/script.js
+++ small/watch/script.js
@@ -1,3 +1,27 @@
+var watchImage = document.getElementById("watch");
+
+var black = document.getElementById("black");
+var red = document.getElementById("red");
+var blue = document.getElementById("blue");
+var pink = document.getElementById("pink");
+var purple = document.getElementById("purple");
+
+black.onclick = function () {
+  watchImage.setAttribute("src", "watch/black.png");
+};
+red.onclick = function () {
+  watchImage.setAttribute("src", "watch/red.png");
+};
+blue.onclick = function () {
+  watchImage.setAttribute("src", "watch/blue.png");
+};
+pink.onclick = function () {
+  watchImage.setAttribute("src", "watch/pink.png");
+};
+purple.onclick = function () {
+  watchImage.setAttribute("src", "watch/purple.png");
+};
+
 var buyBtn = document.getElementById("buy-btn");
 
 buyBtn.onclick = function () {
