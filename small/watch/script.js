9a1af1f3d5107634599a7940b49e9edf1bd9e47b
Akshaypmna18 <akshaypmna18@gmail.com>
2023-07-21 13:15:11 +0530

    updates

--- small/watch/script.js
+++ small/watch/script.js
@@ -1,5 +1,6 @@
 var watchImage = document.getElementById("watch");
 
+var color = document.getElementById("color-name");
 var black = document.getElementById("black");
 var red = document.getElementById("red");
 var blue = document.getElementById("blue");
@@ -8,18 +9,40 @@ var purple = document.getElementById("purple");
 
 black.onclick = function () {
   watchImage.setAttribute("src", "watch/black.png");
+  color.innerHTML = "Black";
 };
 red.onclick = function () {
   watchImage.setAttribute("src", "watch/red.png");
+  color.innerHTML = "Red";
 };
 blue.onclick = function () {
   watchImage.setAttribute("src", "watch/blue.png");
+  color.innerHTML = "Blue";
 };
 pink.onclick = function () {
   watchImage.setAttribute("src", "watch/pink.png");
+  color.innerHTML = "Pink";
 };
 purple.onclick = function () {
   watchImage.setAttribute("src", "watch/purple.png");
+  color.innerHTML = "Purple";
+};
+
+var feature = document.getElementById("feature-name");
+var heartRate = document.getElementById("heart-rate");
+var heartBtn = document.getElementById("heart-rate-btn");
+var timeBtn = document.getElementById("time-btn");
+var time = document.getElementById("time");
+
+heartBtn.onclick = function () {
+  heartRate.classList.remove("display");
+  time.classList.add("display");
+  feature.innerHTML = "Heart Rate";
+};
+timeBtn.onclick = function () {
+  heartRate.classList.add("display");
+  time.classList.remove("display");
+  feature.innerHTML = "Time";
 };
 
 var buyBtn = document.getElementById("buy-btn");
