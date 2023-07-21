15c43704d7b68ee77530587b1ea0f4a4ae5fdbdd
Akshaypmna18 <akshaypmna18@gmail.com>
2023-07-21 17:15:00 +0530

    updated

--- small/music-player/script.js
+++ small/music-player/script.js
@@ -1,5 +1,8 @@
 var playBtn = document.getElementById("btn");
 
+var boxShadow = document.querySelector("#container");
+var textShadow = document.getElementsByTagName("h1");
+
 var music = document.getElementById("music");
 var songName = document.getElementById("song-name");
 var songImage = document.getElementById("song-image");
@@ -14,16 +17,19 @@ var songs = [
       "https://cdns-images.dzcdn.net/images/cover/7e3d2297e30d28e8be3faafc91109df4/500x500.jpg",
     name: "Harleys in Hawaii",
     artist: "Katy Pery",
+    shadow: "#e79099",
   },
   {
     image: "https://i.scdn.co/image/ab67616d0000b273197e8611178672a1e4e0b076",
     name: "Living Life, In The Night",
     artist: "Sierra Kidd, Cheriimoya",
+    shadow: "#0b0b0b",
   },
   {
     image: "https://i.scdn.co/image/ab67616d0000b273fa2a62fe48c2c929c834c084",
     name: "High School",
     artist: "Nicki Minaj",
+    shadow: "#30163b",
   },
 ];
 
