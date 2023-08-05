03c6a645a65a5bbd235268871e96340f5994a8ad
Akshaypmna18 <akshaypmna18@gmail.com>
2023-08-05 07:49:38 +0530

    updates

--- small/formValidation/script.js
+++ small/formValidation/script.js
@@ -13,12 +13,16 @@ userName.addEventListener("input", function () {
   var input = userName.value;
   errorMessages.forEach((error) => {
     error.style.display = "block";
+    errorMessages[2].style.display = "none";
   });
   if (/^[a-zA-Z_]/.test(input)) {
     errorMessages[0].style.display = "none";
   }
-  if (/[a-zA-z]{3,20}/.test(input)) {
+  if (/[a-zA-z]{3,}/.test(input)) {
     errorMessages[1].style.display = "none";
+  }
+  if (/[ @!#$%^&*=+]/.test(input)) {
+    errorMessages[2].style.display = "block";
   }
 });
 
@@ -37,19 +41,19 @@ password.addEventListener("input", function () {
     error.style.display = "block";
   });
   if (/[a-z]/.test(input)) {
-    errorMessages[2].style.display = "none";
-  }
-  if (/[A-Z]/.test(input)) {
     errorMessages[3].style.display = "none";
   }
-  if (/[!@#$%^&*_+=\-]/.test(input)) {
+  if (/[A-Z]/.test(input)) {
     errorMessages[4].style.display = "none";
   }
-  if (/[0-9]/.test(input)) {
+  if (/[!@#$%^&*_+=\-]/.test(input)) {
     errorMessages[5].style.display = "none";
   }
-  if (/[a-zA-Z0-9!@#$%^&*_+=\-]{8}/.test(input)) {
+  if (/[0-9]/.test(input)) {
     errorMessages[6].style.display = "none";
+  }
+  if (/[a-zA-Z0-9!@#$%^&*_+=\-]{8}/.test(input)) {
+    errorMessages[7].style.display = "none";
   }
 });
 eye.onclick = function () {
@@ -64,17 +68,10 @@ eye.onclick = function () {
 
 var phone = document.getElementById("phone");
 var phoneError = document.getElementById("phone-error");
-phone.addEventListener("focus", function () {
-  phoneError.style.display = "block";
-});
-phone.addEventListener("blur", function () {
-  phoneError.style.display = "none";
-});
 phone.addEventListener("input", function () {
   var input = phone.value;
+  phoneError.style.display = "none";
   if (/[a-zA-Z!@#$%^&*_=\-]/.test(input)) {
-    errorMessages[7].style.display = "block";
-  } else {
-    errorMessages[7].style.display = "none";
+    phoneError.style.display = "block";
   }
 });
