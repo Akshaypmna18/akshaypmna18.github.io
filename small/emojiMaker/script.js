3eb165edbd0fed391d95c4ddf70f07ff3bf1dd02
Akshaypmna18 <akshaypmna18@gmail.com>
2023-08-25 13:58:56 +0530

    New project

---
+++ small/emojiMaker/script.js
@@ -0,0 +1,43 @@
+const emoji = $("#emoji");
+
+function changeBg(color) {
+  emoji.css("background-color", color);
+}
+
+function changeEyes(img) {
+  $("#emoji-eyes").attr("src", img);
+}
+
+function changeMouth(img) {
+  $("#emoji-mouth").attr("src", img);
+}
+
+let sections = ["#color-box", "#eyes-box", "#mouth-box"];
+let currentSection = 0;
+
+function toggleSectionsForward() {
+  $(sections[currentSection]).addClass("d-none");
+  currentSection = (currentSection + 1) % sections.length;
+  updateToggleButtons();
+  $(sections[currentSection]).removeClass("d-none");
+}
+
+function toggleSectionsBackward() {
+  $(sections[currentSection]).addClass("d-none");
+  currentSection = (currentSection - 1 + sections.length) % sections.length;
+  updateToggleButtons();
+  $(sections[currentSection]).removeClass("d-none");
+}
+
+function updateToggleButtons() {
+  $("#left-toggle").toggleClass("d-none", currentSection === 0);
+  $("#right-toggle").toggleClass(
+    "d-none",
+    currentSection === sections.length - 1
+  );
+}
+
+$("#right-toggle").click(toggleSectionsForward);
+$("#left-toggle").click(toggleSectionsBackward);
+
+updateToggleButtons();
