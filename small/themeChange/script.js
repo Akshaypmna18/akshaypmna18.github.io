4b4460670c92a00fb5a9c179eb614f399cbaa451
Akshaypmna18 <akshaypmna18@gmail.com>
2023-08-25 19:46:18 +0530

    updates

--- small/themeChange/script.js
+++ small/themeChange/script.js
@@ -1,14 +1,17 @@
 const menuIcon = document.getElementById("menu-icon");
 const links = document.querySelector("nav>ul");
+const listItems = document.querySelectorAll("nav li");
 menuIcon.addEventListener("click", function () {
   links.classList.toggle("display");
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].classList.toggle("transition");
+  }
   if (menuIcon.classList.contains("fa-bars")) {
     menuIcon.classList.replace("fa-bars", "fa-xmark");
   } else {
     menuIcon.classList.replace("fa-xmark", "fa-bars");
   }
 });
-
 const themeIcon = document.getElementById("theme-icon");
 themeIcon.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
@@ -20,13 +23,10 @@ themeIcon.addEventListener("click", function () {
     localStorage.setItem("theme", "light");
   }
 });
-
 if (localStorage.getItem("theme") == "null") {
   localStorage.setItem("theme", "light");
 }
-
 let localData = localStorage.getItem("theme");
-
 if (localData == "light") {
   themeIcon.classList.replace("fa-sun", "fa-moon");
   localStorage.setItem("theme", "light");
