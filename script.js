806d73388a153c398a7be90ab4cb2fb0ebcb0621
Akshaypmna18 <akshaypmna18@gmail.com>
2023-09-19 19:58:48 +0530

    bug fix

--- script.js
+++ script.js
@@ -11,14 +11,9 @@ function createIntersectionObserver(className) {
 }
 
 const animCards = createIntersectionObserver("show-card");
-const animMainHeading = createIntersectionObserver("tracking-in-contract-bck");
 
 const hiddenCards = $("a");
-const hiddenMainHeading = $(" h1");
 
 hiddenCards.each(function () {
   animCards.observe(this);
-});
-hiddenMainHeading.each(function () {
-  animMainHeading.observe(this);
 });
